73e4a4b1e1dbda57a9608a2891f0a385c0bca6b4
HuyNguyen1367 <128899842+HuyNguyen1367@users.noreply.github.com>
2023-04-27 23:39:13 +0700

    Update Bai_1_C++.cpp

--- Bai_1_C++.cpp
+++ Bai_1_C++.cpp
@@ -1,24 +1,88 @@
+/*
+    Bài 1 : 
+    Tạo một lớp điểm trong hệ tọa độ 2D và viết các phương thức để tính
+    khoảng cách giữa hai điểm, tính diện tích của tam giác được tạo bởi ba
+    điểm
+*/
+
 #include<stdint.h>
 #include<stdio.h>
 #include<string>
+#include <iostream>
+#include <math.h>
 
-class Poin
+using namespace std;
+
+class Point 
 {
     private:
-       uint8_t toaDoX;
-       uint8_t toaDoY;
+       int point_X;
+       int point_Y;
     public:
-       Poin(int8_t toaDoX , int8_t toaDoY);   
+       Point(int point_X = 0, int point_Y = 0);
+       int get_Point_X();
+       int get_poin_Y();  
 };
 
-Poin::Poin(int8_t toaDoX , int8_t toaDoY)
+int Point::get_Point_X()
 {
-    Poin::toaDoX = toaDoX;
-    Poin::toaDoY = toaDoY;
+    return Point::point_X;
+}
+
+int Point::get_poin_Y()
+{
+    return Point::point_Y;
+}
+
+Point::Point(int point_X , int point_Y)
+{
+    Point::point_X = point_X;   
+    Point::point_Y = point_Y;
+}
+
+class Math_Point
+{
+    private:
+       Point point_A;
+       Point point_B;
+       Point point_C;
+       double Distance_TwoPoint = 0;           
+       double Area_Triangle = 0;
+    public:
+       Math_Point(Point point_A , Point point_B  );
+       Math_Point(Point point_A , Point point_B , Point point_C );
+       double Calculation_distance();                                              
+       double Calculation_area(); 
+    
+};
+
+Math_Point::Math_Point(Point point_A , Point point_B  )
+{
+    Math_Point::point_A = point_A;
+    Math_Point::point_B = point_B;
+}
+
+Math_Point::Math_Point(Point point_A , Point point_B , Point point_C )
+{
+    Math_Point::point_A = point_A;
+    Math_Point::point_B = point_B;
+    Math_Point::point_C = point_C;
+}
+
+double Math_Point::Calculation_distance()
+{
+    Math_Point::Distance_TwoPoint = sqrt( pow((point_A.get_Point_X() - point_B.get_Point_X()) , 2 ) + pow((point_A.get_poin_Y() - point_B.get_poin_Y()) , 2 ) );
+    return Math_Point::Distance_TwoPoint;
 }
 
 int main(int argc, char const *argv[])
 {
-   
+    Point first(1,2);
+    Point second(3,4);
+
+    Math_Point distance_point(first,second);
+    printf("%lf",distance_point.Calculation_distance());
+    
     return 0;
 }
+
